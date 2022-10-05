c14fe464b94cccb74e8ae86f6af74a505bd12818
Floyd Hightower <floyd.hightower27@gmail.com>
2022-10-05 17:00:28 -0400

    Collect some inputs from cli

--- iolaus.go
+++ iolaus.go
@@ -1,24 +1,63 @@
 package main
 
 import (
-    "context"
+	"context"
+	"flag"
 	"fmt"
 	"github.com/google/go-github/github"
-    "golang.org/x/oauth2"
+	"golang.org/x/oauth2"
+	"strings"
 )
 
+func getCliArgs() (string, []string) {
+	var apiToken, repoListString string
+
+	flag.StringVar(&apiToken, "t", "", "API token")
+	flag.StringVar(&repoListString, "r", "", "Comma separated list of repos")
+	flag.Parse()
+
+	repoList := strings.Split(repoListString, ",")
+
+	return apiToken, repoList
+}
+
+func validateCliArgs(apiToken string, repoList []string) bool {
+	errors := false
+
+	// todo: this does not work... repoList has a length of 1 even when no repos are provided... look into why this is
+	if len(repoList) == 0 {
+		errors = true
+		fmt.Println("Please provide repos to parse")
+	}
+
+	if apiToken == "" {
+		errors = true
+		fmt.Println("Please provide a personal access token: https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token")
+	}
+
+	return errors
+}
+
 func main() {
-	fmt.Println("hi!")
-    ctx := context.Background()
+	apiToken, repoList := getCliArgs()
+	errors := validateCliArgs(apiToken, repoList)
+
+	if errors {
+		return
+	}
+
+	// todo: this is just for testing...
+	return
+
+	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: ""},
+		&oauth2.Token{AccessToken: apiToken},
 	)
 	tc := oauth2.NewClient(ctx, ts)
-
-	client := github.NewClient(tc)
+	ghClient := github.NewClient(tc)
 
 	// list all repositories for the authenticated user
-	repos, _, err := client.Repositories.List(ctx, "", nil)
-    fmt.Println(repos)
-    fmt.Println(err)
+	repos, _, err := ghClient.Repositories.List(ctx, "", nil)
+	fmt.Println(repos)
+	fmt.Println(err)
 }
