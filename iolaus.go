a0df6ff0939a3bc82043bbc781d5fbad3e5a8c4c
Floyd Hightower <floyd.hightower27@gmail.com>
2022-10-05 16:16:42 -0400

    Add basic script

---
+++ iolaus.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "context"
+	"fmt"
+	"github.com/google/go-github/github"
+    "golang.org/x/oauth2"
+)
+
+func main() {
+	fmt.Println("hi!")
+    ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: ""},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	// list all repositories for the authenticated user
+	repos, _, err := client.Repositories.List(ctx, "", nil)
+    fmt.Println(repos)
+    fmt.Println(err)
+}
